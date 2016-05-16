131c9b03cb974c580d23a55ace6df1aaa10190f4
cod3g3nki <adeeldev@gmail.com>
2016-05-17 05:06:57 +0800

    removed make_foo.sh

--- w02/mm-record-update.c
+++ w02/mm-record-update.c
@@ -1,5 +1,6 @@
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 struct record {
@@ -9,20 +10,37 @@ struct record {
 
 int main()
 {
-  int fd;
+  int fd, numrec;
   size_t size;
   struct record *records;
 
   fd = open("datafile1", O_RDWR);
-  size = lseek(fd, 0, SEEK_END);
-  lseek(fd, 0, SEEK_SET);
-  records = (struct record *)mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-  records[1].id = 111;
+  if (fd < 0) {
+    perror("datafile1");
+    exit(1);
+  }
 
-  // msync(records, size, MS_SYNC);
+  size = lseek(fd, 0, SEEK_END);
+  numrec = size / sizeof(struct record);
+  printf("Reading %d bytes / %d records from file.\n", (int)size, numrec);
+  lseek(fd, 0, SEEK_SET);
+
+  records = (struct record *)mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (records == MAP_FAILED) {
+      perror("mmap");
+      exit(2);
+  }
+
+  printf("Updating current value of record[1] id: %d\n", records[1].id);
+  records[1].id = 2;
+
   if (msync(records, size, MS_SYNC) == -1) {
       perror("msync");
       exit(5);
   }
+
+  munmap(records, size);
   close(fd);
+
+  return 0;
 }
